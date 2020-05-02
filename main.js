2ae912b97e72ae6a2627571259779f5668ab527d
Andi <63165028+Andi-Tafilaj@users.noreply.github.com>
2020-05-01 21:00:42 -0400

    Update main.js

--- main.js
+++ main.js
@@ -19,13 +19,19 @@ clients[0].once('ready', () => {
 	clients[0].user.setActivity("Astronomia", { type: "LISTENING"});
 });
 
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
 clients[0].on('message', async (message) => {
+	let guildName = message.guild.name;
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
 
-	if(message.content === (`${prefix}join`) && message.member.hasPermission("ADMINISTRATOR"))
+	if(command === 'join' && message.member.hasPermission("ADMINISTRATOR"))
 	{	
-		let channel_info = message.member.guild.voiceStates.cache.find(user => user.id == message.author.id)
+		console.log("Command for " + command + " Recieved in Server: " + guildName)
 
-		console.log("Command Recieved in Channel: " + message.member.voice.channel.id)
 		const connection = await message.member.voice.channel.join();
 		const dispatcher = connection.play('./Astronomia.mp3');
 
@@ -38,11 +44,38 @@ clients[0].on('message', async (message) => {
 			connection.disconnect();
 		});
 	}
-	if(message.content === (`${prefix}leave`) && message.member.hasPermission("ADMINISTRATOR"))
+	if(command === 'ban' && message.member.hasPermission("BAN_MEMBERS"))	//Someone please fix this mess
+	{
+		console.log("Command for " + command + " Recieved in Server: " + guildName)
+
+		const connection = await message.member.voice.channel.join();
+		const dispatcher = connection.play('./Astronomia.mp3');
+
+		let member = message.mentions.members.first();
+		if(!member)
+			return message.reply("Please mention a valid member of this server");
+		if(!member.bannable) 
+			return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
+
+		let reason = args.slice(1).join(' ');
+		if(!reason) reason = "No reason provided";
+
+		dispatcher.on('start', () => {
+			console.log('Astronomia now playing in');
+		});
+
+		dispatcher.on('finish', () => {
+			console.log('Astronomia done playing');
+			member.send("You have been banned from " + guildName + ` for \"${reason}\"`);
+			member.ban(reason);
+			connection.disconnect();
+		});		
+	}
+	if(command === 'leave' && message.member.hasPermission("ADMINISTRATOR"))
 		{
 			message.member.voice.channel.leave();	
 		}
-	if(message.content === (`${prefix}help`) && message.member.hasPermission("ADMINISTRATOR"))
+	if(command === 'help' && message.member.hasPermission("ADMINISTRATOR"))
 	{
 		const helpEmbed = new Discord.MessageEmbed()
                 .setColor('#0099ff')
@@ -53,20 +86,18 @@ clients[0].on('message', async (message) => {
                 .addFields(
 					{ name: '- !g help', value: 'This command...' },
 					{ name: '- !g join', value: 'Joins the voice channel you are in' },
-					{ name: '- !g leave', value: 'Leaves the voice channel incase needed!' }
+					{ name: '- !g leave', value: 'Leaves the voice channel incase needed!' },
+					{ name: '- !g ban <tag user>', value: 'Bans the user while music is playing! Will DM the user that he has been banned.' }
                 );
             return message.channel.send(helpEmbed);
 
 	}
 })
 
- function sleep(ms) {
-	return new Promise(resolve => setTimeout(resolve, ms));
-  }
-
 clients[1].on('message', async (message) => {
-
-	if(message.content === (`${prefix}join`) && message.member.hasPermission("ADMINISTRATOR"))
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+	if((command === 'join' && message.member.hasPermission("ADMINISTRATOR")) || (command === 'ban' && message.member.hasPermission("BAN_MEMBERS")))
 		{
 			await sleep(19500);
 			console.log("Client 1 is joining voice chat");
@@ -75,15 +106,16 @@ clients[1].on('message', async (message) => {
 			connection.disconnect();
 			console.log("Client 1 has left voice chat");
 		}
-		if(message.content === (`${prefix}leave`))
+		if(command === 'leave' && message.member.hasPermission("ADMINISTRATOR"))
 		{
 			message.member.voice.channel.leave();	
 		}
 })
 
 clients[2].on('message', async (message) => {
-
-	if(message.content === (`${prefix}join`) && message.member.hasPermission("ADMINISTRATOR"))
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+	if((command === 'join' && message.member.hasPermission("ADMINISTRATOR")) || (command === 'ban' && message.member.hasPermission("BAN_MEMBERS")))
 	{
 		await sleep(19500);
 		console.log("Client 2 is joining voice chat");
@@ -92,7 +124,7 @@ clients[2].on('message', async (message) => {
 		connection.disconnect();
 		console.log("Client 2 has left voice chat");
 	}
-	if(message.content === (`${prefix}leave`) && message.member.hasPermission("ADMINISTRATOR"))
+	if(command === 'leave' && message.member.hasPermission("ADMINISTRATOR"))
 	{
 		message.member.voice.channel.leave();	
 	}
@@ -100,8 +132,9 @@ clients[2].on('message', async (message) => {
 
 
 clients[3].on('message', async (message) => {
-
-	if(message.content === (`${prefix}join`) && message.member.hasPermission("ADMINISTRATOR"))
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+	if((command === 'join' && message.member.hasPermission("ADMINISTRATOR")) || (command === 'ban' && message.member.hasPermission("BAN_MEMBERS")))
 	{
 		await sleep(19500);
 		console.log("Client 3 is joining voice chat");
@@ -110,7 +143,7 @@ clients[3].on('message', async (message) => {
 		connection.disconnect();
 		console.log("Client 3 has left voice chat");
 	}
-	if(message.content === (`${prefix}leave`) && message.member.hasPermission("ADMINISTRATOR"))
+	if(command === 'leave' && message.member.hasPermission("ADMINISTRATOR"))
 	{
 		message.member.voice.channel.leave();	
 	}
@@ -118,8 +151,9 @@ clients[3].on('message', async (message) => {
 
 
 clients[4].on('message', async (message) => {
-
-	if(message.content === (`${prefix}join`) && message.member.hasPermission("ADMINISTRATOR"))
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+	if((command === 'join' && message.member.hasPermission("ADMINISTRATOR")) || (command === 'ban' && message.member.hasPermission("BAN_MEMBERS")))
 	{
 		await sleep(19500);
 		console.log("Client 4 is joining voice chat");
@@ -128,7 +162,7 @@ clients[4].on('message', async (message) => {
 		connection.disconnect();
 		console.log("Client 4 has left voice chat");
 	}
-	if(message.content === (`${prefix}leave`) && message.member.hasPermission("ADMINISTRATOR"))
+	if(command === 'leave' && message.member.hasPermission("ADMINISTRATOR"))
 	{
 		message.member.voice.channel.leave();	
 	}
@@ -136,8 +170,9 @@ clients[4].on('message', async (message) => {
 
 
 clients[5].on('message', async (message) => {
-
-	if(message.content === (`${prefix}join`) && message.member.hasPermission("ADMINISTRATOR"))
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+	if((command === 'join' && message.member.hasPermission("ADMINISTRATOR")) || (command === 'ban' && message.member.hasPermission("BAN_MEMBERS")))
 	{
 		await sleep(19500);
 		console.log("Client 5 is joining voice chat");
@@ -146,7 +181,7 @@ clients[5].on('message', async (message) => {
 		connection.disconnect();
 		console.log("Client 5 has left voice chat");
 	}
-	if(message.content === (`${prefix}leave`) && message.member.hasPermission("ADMINISTRATOR"))
+	if(command === 'leave' && message.member.hasPermission("ADMINISTRATOR"))
 	{
 		message.member.voice.channel.leave();	
 	}
@@ -154,8 +189,9 @@ clients[5].on('message', async (message) => {
 
 
 clients[6].on('message', async (message) => {
-
-	if(message.content === (`${prefix}join`) && message.member.hasPermission("ADMINISTRATOR"))
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+	if((command === 'join' && message.member.hasPermission("ADMINISTRATOR")) || (command === 'ban' && message.member.hasPermission("BAN_MEMBERS")))
 	{
 		await sleep(19500);
 		console.log("Client 6 is joining voice chat");
@@ -164,7 +200,7 @@ clients[6].on('message', async (message) => {
 		connection.disconnect();
 		console.log("Client 6 has left voice chat");
 	}
-	if(message.content === (`${prefix}leave`) && message.member.hasPermission("ADMINISTRATOR"))
+	if(command === 'leave' && message.member.hasPermission("ADMINISTRATOR"))
 	{
 		message.member.voice.channel.leave();	
 	}
